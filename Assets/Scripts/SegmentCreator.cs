47aa2cbacfdb5b66007fff28cb887e92379b454f
Jakub Kuczys <me@jacken.men>
2023-09-04 03:02:28 +0200

    Handle nullability of fields in SegmentCreator

--- Assets/Scripts/SegmentCreator.cs
+++ Assets/Scripts/SegmentCreator.cs
@@ -31,6 +31,11 @@ public class SegmentCreator : MonoBehaviour
 
     public void StartCreatingSegment(Point originPoint)
     {
+        if (_startingPoint is not null)
+        {
+            _startingPoint.IsSelected = false;
+            _startingPoint = null;
+        }
         _currentSegment = Instantiate(_segmentPrefab, _segmentsParent).transform;
         _currentSegment.position = originPoint.transform.position;
         EditSpaceController.Instance.CurrentMode = Mode.CONNECT_POINTS;
@@ -40,8 +45,11 @@ public class SegmentCreator : MonoBehaviour
 
     public void StopCreatingSegment(Point endPoint)
     {
-        if (_startingPoint == endPoint)
+        if (_startingPoint is null || _startingPoint == endPoint)
+        {
             return;
+        }
+
         TrackSegment(endPoint.transform);
         _currentSegment.localScale = new Vector3(
             _currentSegment.localScale.x,
@@ -51,10 +59,15 @@ public class SegmentCreator : MonoBehaviour
         _currentSegment = null;
         EditSpaceController.Instance.CurrentMode = Mode.CREATE_POINT;
         _startingPoint.IsSelected = false;
+        _startingPoint = null;
     }
 
     public void TrackSegment(Transform toFollow)
     {
+        if (_currentSegment is null)
+        {
+            return;
+        }
         _currentSegment.LookAt(toFollow);
         _currentSegment.RotateAround(_currentSegment.position, _currentSegment.right, 90);
         _currentSegment.localScale = new Vector3(
