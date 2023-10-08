5ece19b1fae6adb725803e8d9dfa7574d87e9501
AaronWantoch <58564314+AaronWantoch@users.noreply.github.com>
2023-10-09 00:02:33 +0200

    Point segment creation
    Project basics (creating points and segments)
    
    ---------
    
    Co-authored-by: Jakub Kuczys <me@jacken.men>

---
+++ Assets/Scripts/PointCreator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCreator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pointPrefab;
+
+    [SerializeField]
+    private Transform _pointsParent;
+    public static PointCreator Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public Point CreatePoint(Vector3 positionToCreate)
+    {
+        return Instantiate(_pointPrefab, positionToCreate, Quaternion.identity, _pointsParent)
+            .GetComponent<Point>();
+    }
+}
