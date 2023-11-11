4949c4f26b205408e0b799b4927c25cc35e2efca
AaronWantoch <58564314+AaronWantoch@users.noreply.github.com>
2023-11-11 16:41:19 +0000

    Camera movement (#5)
    Co-authored-by: Jakub Kuczys <me@jacken.men>

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField]
+    private float minRadius = 1f;
+
+    [SerializeField]
+    private float maxRadius = 10f;
+
+    [SerializeField]
+    private Transform centrePoint;
+
+    private float _width;
+    private float _height;
+
+    private bool _flipped;
+    private float _currentRadius;
+    private float _currentPolar;
+    private float _currentElevation;
+    private Vector3 _newPosition;
+
+    void Start()
+    {
+        _width = Screen.width / 2f;
+        _height = Screen.height / 2f;
+        CalculateStartingSphericalCoordinates();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (Input.touchCount == 2)
+            {
+                HandleZoom(touch);
+            }
+            else
+            {
+                MoveCamera(touch);
+            }
+            UpdateCartesian();
+            transform.position = _newPosition;
+
+            _flipped = false;
+            Vector3 direction = Vector3.up;
+            if (_currentElevation > Mathf.PI / 2 || _currentElevation < -Mathf.PI / 2)
+            {
+                direction = Vector3.down;
+                _flipped = true;
+            }
+            transform.LookAt(centrePoint, direction);
+        }
+    }
+
+    private void HandleZoom(Touch touch)
+    {
+        Touch touchOne = Input.GetTouch(1);
+        Vector2 touchZeroPreviousPosition = touch.position - touch.deltaPosition;
+        Vector2 touchOnePreviousPosition = touchOne.position - touchOne.deltaPosition;
+        float prevTouchDeltaMag = (touchZeroPreviousPosition - touchOnePreviousPosition).magnitude;
+        float TouchDeltaMag = (touch.position - touchOne.position).magnitude;
+        float deltaMagDiff = prevTouchDeltaMag - TouchDeltaMag;
+        _currentRadius = Mathf.Clamp(
+            _currentRadius + deltaMagDiff * Time.deltaTime,
+            minRadius,
+            maxRadius
+        );
+    }
+
+    private void MoveCamera(Touch touch)
+    {
+        // Move the cube if the screen has the finger moving.
+        if (touch.phase == TouchPhase.Moved)
+        {
+            int xMultiplier = 1;
+            if (_flipped)
+                xMultiplier = -1;
+            Vector2 pos = touch.deltaPosition;
+            _currentPolar += -pos.x * xMultiplier * speed * Time.deltaTime;
+            _currentElevation += -pos.y * speed * Time.deltaTime;
+
+            // polar and elevation should be in range from -PI to PI
+            if (_currentPolar > Mathf.PI)
+                _currentPolar -= 2 * Mathf.PI;
+            if (_currentElevation > Mathf.PI)
+                _currentElevation -= 2 * Mathf.PI;
+            if (_currentPolar < -Mathf.PI)
+                _currentPolar += 2 * Mathf.PI;
+            if (_currentElevation < -Mathf.PI)
+                _currentElevation += 2 * Mathf.PI;
+        }
+    }
+
+    private void UpdateCartesian()
+    {
+        float a = _currentRadius * Mathf.Cos(_currentElevation);
+        _newPosition.x = a * Mathf.Cos(_currentPolar) + centrePoint.position.x;
+        _newPosition.y = _currentRadius * Mathf.Sin(_currentElevation) + centrePoint.position.y;
+        _newPosition.z = a * Mathf.Sin(_currentPolar) + centrePoint.position.z;
+    }
+
+    private void CalculateStartingSphericalCoordinates()
+    {
+        Vector3 cartCoords = transform.position;
+        if (cartCoords.x == 0)
+            cartCoords.x = Mathf.Epsilon;
+        _currentRadius = Mathf.Sqrt(
+            (cartCoords.x * cartCoords.x)
+                + (cartCoords.y * cartCoords.y)
+                + (cartCoords.z * cartCoords.z)
+        );
+        _currentPolar = Mathf.Atan(cartCoords.z / cartCoords.x);
+        if (cartCoords.x < 0)
+            _currentPolar += Mathf.PI;
+        _currentElevation = Mathf.Asin(cartCoords.y / _currentRadius);
+    }
+}
