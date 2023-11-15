d4a0b9a4633f80eeb49e468de1957afc91a8bc19
Jakub Kuczys <me@jacken.men>
2023-11-15 03:33:06 +0100

    Remove unused usings

--- Assets/Scripts/UIHandlers/CreatePolygonMenu/Coordinates.cs
+++ Assets/Scripts/UIHandlers/CreatePolygonMenu/Coordinates.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.Serialization;
 
 namespace StereoApp.UIHandlers.CreatePolygonMenu
 {
