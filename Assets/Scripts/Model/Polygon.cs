fc369453139530815d9baecf487b4cda8ec5af49
Jakub Kuczys <me@jacken.men>
2023-12-08 01:43:51 +0100

    Use a more suitable for Polygon.ReplaceAll()
    As a side effect, this also fixes an issue with PolygonPresenter
    not being triggered by this method due to use of the Reset event.

--- Assets/Scripts/Model/Polygon.cs
+++ Assets/Scripts/Model/Polygon.cs
@@ -16,7 +16,7 @@ namespace StereoApp.Model
             INotifyPropertyChanged
     {
         private const float CoplanarTolerance = 1e-5f;
-        private readonly List<Point> _points;
+        private List<Point> _points;
 
         public IEnumerable<Segment> Segments =>
             _points.Select((point, i) => new Segment(point, _points[(i + 1) % _points.Count]));
@@ -176,12 +176,16 @@ namespace StereoApp.Model
             );
         }
 
-        public void ReplaceAll(List<Point> points)
+        public void ReplaceAll(IEnumerable<Point> points)
         {
-            _points.Clear();
-            _points.AddRange(points);
+            var oldItems = _points;
+            _points = new List<Point>(points);
             OnCollectionChanged(
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset)
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Replace,
+                    oldItems,
+                    _points.AsReadOnly()
+                )
             );
         }
 
