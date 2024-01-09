60b8841dba9a0e465ffada3c807cc4f6f40b58d5
Jakub Kuczys <me@jacken.men>
2024-01-09 13:02:29 +0100

    Allow creating a new project once a figure is generated (#23)
    I'm not entirely sure that there isn't some state somewhere that
    persists through this but I haven't noticed any issues with it in quick
    testing.

--- Assets/Scripts/Presenter/UIHandlers/ToolbarMenu/ToolbarMenuManager.cs
+++ Assets/Scripts/Presenter/UIHandlers/ToolbarMenu/ToolbarMenuManager.cs
@@ -28,6 +28,7 @@ namespace StereoApp.Presenter.UIHandlers.ToolbarMenu
         public CreateFacesMenuHandler facesMenu;
         public CreatePolygonMenuHandler polygonMenu;
         public RectTransform nameThingsMenu;
+        public RectTransform newProjectConfirmMenu;
         public NameSegmentsMenu nameSegmentsMenu;
         public NameAnglesMenu nameAnglesMenu;
 
@@ -97,6 +98,11 @@ namespace StereoApp.Presenter.UIHandlers.ToolbarMenu
             SwitchToMenu(nameThingsMenu);
         }
 
+        public void ShowNewProjectConfirmMenu()
+        {
+            SwitchToMenu(newProjectConfirmMenu);
+        }
+
         public void ShowNameSegmentsMenu()
         {
             SwitchToMenu(nameSegmentsMenu);
@@ -113,6 +119,7 @@ namespace StereoApp.Presenter.UIHandlers.ToolbarMenu
             polygonMenu.gameObject.SetActive(false);
             mainMenu.gameObject.SetActive(false);
             nameThingsMenu.gameObject.SetActive(false);
+            newProjectConfirmMenu.gameObject.SetActive(false);
             nameSegmentsMenu.gameObject.SetActive(false);
             nameAnglesMenu.gameObject.SetActive(false);
         }
